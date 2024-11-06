1f6fafef4bb1d69efaca8df8f2d2d4bd120d3627
Florian <florian.soulas1@gmail.com>
2024-11-06 16:41:34 +0100

    cart js mod

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,13 +1,12 @@
 // GET /CART -> affiche mes cart
 fetch('http://localhost:3000/cart')
-	.then(response => response.json())
+    .then(response => response.json())
     .then(cartData => {
         // console.log(cartData)
 
-        if(cartData){
-        // console.log("full")
+        if (cartData) {
 
-        document.querySelector('.cart-content').innerHTML += `
+            document.querySelector('.cart-content').innerHTML += `
 			<div class="cart-result">
                 <h2>My cart</h2>
 
@@ -26,19 +25,13 @@ fetch('http://localhost:3000/cart')
 			`;
             let priceTotal = 0;
 
-			for (let i = 0; i < cartData.cartResult.length; i++) {
-            let departure = cartData.cartResult[i].departure;
-            let arrival = cartData.cartResult[i].arrival;
-            let date = new Date(cartData.cartResult[i].date);
-            let hours = date.getHours();
-            let minutes = date.getMinutes();
-            let stringDate = `${hours} : ${minutes}`
-            let price = cartData.cartResult[i].price;
-            console.log(cartData.cartResult[i].price)
-            priceTotal += price
-
-            console.log(priceTotal)
-
+            for (let i = 0; i < cartData.cartResult.length; i++) {
+                let departure = cartData.cartResult[i].departure;
+                let stringDate = cartData.cartResult[i].date;
+                console.log(stringDate);
+                let price = cartData.cartResult[i].price;
+                console.log(cartData.cartResult[i].price)
+                priceTotal += price
                 document.querySelector('.wrapper').innerHTML += `
                 <div class="result-wrap"">
                     <p>${departure}</p>
@@ -46,15 +39,15 @@ fetch('http://localhost:3000/cart')
                     <p><span class="price-cart">${price}</span>€</p>
                     <div class="btn-delete-cart">X</div>
                 </div>
-                `;				
-			}
-            
+                `;
+            }
+
             document.querySelector('.price-total-cart').innerHTML += `
             ${priceTotal}€
             `;
-
-		}else{
-            // console.log("empty")
+            purchase();
+            supress();
+        } else {
             document.querySelector('.cart-content').innerHTML += `
 			<div class="cart-empty">
                 <p>No tickets in your cart.<br><br></p>
@@ -62,12 +55,42 @@ fetch('http://localhost:3000/cart')
             </div>
 			`;
         }
-
-	});
-
-  
+    });
 
 
+//PURCHASE
+async function purchase() {
+    document.querySelector('.btn-purchase-cart').addEventListener('click', async function () {
+        try {
+            const response = await fetch('http://localhost:3000/cart/paid')
+            const result = response.json();
+            console.log(result);
+            document.querySelector('.cart-result').remove()
+            document.querySelector('.cart-content').innerHTML += `
+			<div class="cart-empty">
+                <p>No tickets in your cart.<br><br></p>
+                <p>Why not plan a trip?</p>
+            </div>
+			`;
+        } catch (error) {
+            console.error("Erreur de la requête", error);
+        }
+
+    })
+}
+
+//DELETE
+async function supress() {
+    for(let i = 0; i < document.querySelectorAll('.btn-delete-cart').length; i++){
+        document.querySelectorAll('.btn-delete-cart')[i].addEventListener('click', async function() {
+            try {
+                this.parentNode.remove()
+            } catch (error) {
+                console.error("Erreur de la requête", error);
+            }
+        });
+    }
+};
 
 
 
